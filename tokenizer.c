33f0b962faea3fa570000a4ecd76c71d6930808c
davidslayin <dave191994@gmail.com>
2022-09-08 20:00:37 +0300

    fix tokenzir

--- tokenizer.c
+++ tokenizer.c
@@ -1,56 +1,94 @@
 #include "shell.h"
 
 /**
- * wordcount - counts tokens in a string for _strtok
- * @str: string to tokenize
- * @delim: delimeter between tokens
- *
- * Description: loop through string
- * If the current character is a delim, turn flag off
- * Else, turn flag on and increment the count
- *
- * Return: number of tokens
+ * **strtow - splits a string into words. Repeat delimiters are ignored
+ * @str: the input string
+ * @d: the delimeter string
+ * Return: a pointer to an array of strings, or NULL on failure
  */
-int wordcount(char *str, char delim)
-{
-	int num = 0, i;
-	int flag = 0;
 
-	for (i = 0; str[i]; i++)
+char **strtow(char *str, char *d)
+{
+	int i, j, k, m, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	if (!d)
+		d = " ";
+	for (i = 0; str[i] != '\0'; i++)
+		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
+			numwords++;
+
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
 	{
-		if (str[i] == delim)
-			flag = 0;
-		else if (flag == 0)
+		while (is_delim(str[i], d))
+			i++;
+		k = 0;
+		while (!is_delim(str[i + k], d) && str[i + k])
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
 		{
-			flag = 1;
-			num++;
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
 		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
 	}
-	return (num);
+	s[j] = NULL;
+	return (s);
 }
 
 /**
-* _strtok - converts a string into an array of tokens
-* @str: string to tokenize
-* @delim: delimeter between tokens
-*
-* Description: if the string is NULL, return NULL
-* Set total to number of tokens in str
-* If total is 0, return NULL
-* Allocate memory for the array of strings, check if it failed
-* Loop through string and as loop as the increment is smaller than total
-* Store current position of str in cpy
-* Go pass the characters until the delim
-* Allocate memory for the previous token, check if it failed
-* Loop through cpy (copy of the token)
-* Store each character into the array of strings words
-* Terminate the string by a null byte
-* Continue
-* Set the last string of the array to NULL, return array
-*
-* Return: pointer to the array of strings on success, or NULL on failure
-*/
-char **_strtok(char *str, char delim)
+ * **strtow2 - splits a string into words
+ * @str: the input string
+ * @d: the delimeter
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+char **strtow2(char *str, char d)
 {
-	char *cpy = NULL, **words = NULL;
-	int }
+	int i, j, k, m, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] != d && str[i + 1] == d) ||
+		    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
+	{
+		while (str[i] == d && str[i] != d)
+			i++;
+		k = 0;
+		while (str[i + k] != d && str[i + k] && str[i + k] != d)
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = str[i++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
+}
