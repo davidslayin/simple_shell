ca7ce81082888d0c6e50ca768ed0c7c243337588
davidslayin <dave191994@gmail.com>
2022-09-08 19:54:12 +0300

    Update lists.c

--- lists.c
+++ lists.c
@@ -1,113 +1,159 @@
 #include "shell.h"
 
 /**
- * _setenv - sets an environment variable,
- * handles the setenv builtin
- * @head: double pointer to the environ linked list
- * @argv: name and value of the variable to set
- * @args: number of arguments
+ * add_node - adds a node to the start of the list
+ * @head: address of pointer to head node
+ * @str: str field of node
+ * @num: node index used by history
  *
- * Description: If the list is empty, return NULL
- * If no arguments is passed to setenv,
- * print the list
- * If too many arguments are passed, return -1
- * Concatenate the first argument with an '='
- * and the second argument
- * Find the index of the name we're looking for
- * If not found, add the variable at the end
- * Else delete the node at the index, and add the new string
- *
- * Return: 0 on success, -1 on failure
+ * Return: size of list
  */
-int _setenv(env_t **head, char **argv, int args)
+list_t *add_node(list_t **head, const char *str, int num)
 {
-	char *buf1 = NULL, *buf2 = NULL;
-	int index = 0;
+	list_t *new_head;
+
+	if (!head)
+		return (NULL);
+	new_head = malloc(sizeof(list_t));
+	if (!new_head)
+		return (NULL);
+	_memset((void *)new_head, 0, sizeof(list_t));
+	new_head->num = num;
+	if (str)
+	{
+		new_head->str = _strdup(str);
+		if (!new_head->str)
+		{
+			free(new_head);
+			return (NULL);
+		}
+	}
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
+}
+
+/**
+ * add_node_end - adds a node to the end of the list
+ * @head: address of pointer to head node
+ * @str: str field of node
+ * @num: node index used by history
+ *
+ * Return: size of list
+ */
+list_t *add_node_end(list_t **head, const char *str, int num)
+{
+	list_t *new_node, *node;
+
+	if (!head)
+		return (NULL);
+
+	node = *head;
+	new_node = malloc(sizeof(list_t));
+	if (!new_node)
+		return (NULL);
+	_memset((void *)new_node, 0, sizeof(list_t));
+	new_node->num = num;
+	if (str)
+	{
+		new_node->str = _strdup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	}
+	if (node)
+	{
+		while (node->next)
+			node = node->next;
+		node->next = new_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
+
+/**
+ * print_list_str - prints only the str element of a list_t linked list
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+size_t print_list_str(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * delete_node_at_index - deletes node at given index
+ * @head: address of pointer to first node
+ * @index: index of node to delete
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int delete_node_at_index(list_t **head, unsigned int index)
+{
+	list_t *node, *prev_node;
+	unsigned int i = 0;
 
 	if (!head || !*head)
 		return (0);
 
-	if (args != 3)
-		return (-1);
-
-	buf1 = str_concat(argv[1], "=");
-	buf2 = str_concat(buf1, argv[2]);
-	free(buf1);
-
-	index = find_index_list(*head, argv[1]);
-	if (index == 0)
+	if (!index)
 	{
-		add_node_end(head, buf2);
-		free(buf2);
-		return (0);
+		node = *head;
+		*head = (*head)->next;
+		free(node->str);
+		free(node);
+		return (1);
 	}
-
-	if (index > 0)
+	node = *head;
+	while (node)
 	{
-		delete_node_at_index(head, index);
-		add_node_at_index(head, buf2, index);
-		free(buf2);
-		return (0);
+		if (i == index)
+		{
+			prev_node->next = node->next;
+			free(node->str);
+			free(node);
+			return (1);
+		}
+		i++;
+		prev_node = node;
+		node = node->next;
 	}
-
-	free(buf2);
 	return (0);
 }
 
 /**
- * _unsetenv - unsets an environment variable,
- * handles the unsetenv builtin
- * @head: double pointer to the environ linked list
- * @argv: array of strings, includes parsed line
+ * free_list - frees all nodes of a list
+ * @head_ptr: address of pointer to head node
  *
- * Description: If no arguments is passed, return -1
- * Find the index of the variable, delete it
- *
- * Return: 0 on success, -1 on failure
+ * Return: void
  */
-int _unsetenv(env_t **head, char **argv)
+void free_list(list_t **head_ptr)
 {
-	int index;
+	list_t *node, *next_node, *head;
 
-	if (!argv[1])
-		return (-1);
-
-	index = find_index_list(*head, argv[1]);
-	if (index == 0 || !head || !*head)
-		return (0);
-
-	else
-		delete_node_at_index(head, index);
-
-	return (0);
+	if (!head_ptr || !*head_ptr)
+		return;
+	head = *head_ptr;
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node = next_node;
+	}
+	*head_ptr = NULL;
 }
-
-/**
- * setenv_handler - handles the setenv and unsetenv builtins
- * @argv: array of arguments from the command line
- * @head: pointer to the env_t linked list
- * @i: index of the command in history
- * @prog_name: name of the program
- *
- * Description: Count the umber of arguments
- * If setenv, call _setenv, print error on failure
- * If unsetenv, call _unsetenv, print error on failure
- */
-void setenv_handler(char **argv, env_t **head, int *i, char *prog_name)
-{
-	int n = 0, m = 0, args = 0;
-	char *unset_err = "unsetenv: Too few arguments.\n";
-
-	while (argv[args])
-		args++;
-
-	if (!_strcmp(argv[0], "setenv"))
-	{
-		n = _setenv(head, argv, args);
-		if (n == -1)
-			print_error_setenv(i, prog_name, argv);
-	} else if (!_strcmp(argv[0], "unsetenv"))
-	{
-		m = _unsetenv(head, argv);
-		if (m == -1)
-			write(2, uns}
